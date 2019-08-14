96437610190bb2fb9ecc0d635f20704104342e87
Janio <janiojunior@gmail.com>
2019-08-14 09:13:11 -0300

    Criando Projeto

---
+++ src/br/unitins/hello/controller/LoginController.java
@@ -0,0 +1,36 @@
+package br.unitins.hello.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+public class LoginController {
+
+	private String login = "Joao";
+	private String senha = "123";
+
+	public void logar() {
+		System.out.println(login);
+		System.out.println(senha);
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+}
